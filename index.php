141bd2868eedfc78b4648f1938493782c017171c
ALBERTO ALVAREZ <albertoal@users.noreply.github.com>
2017-09-10 11:48:09 -0700

    Updated image path and repo name

--- index.php
+++ index.php
@@ -8,7 +8,7 @@
 				<div align="center">
                 <h1>Welcome to the Demo site</h1>
                 <h2>Congrats, You have successfully deployed a PHP app into Beanstalk!</h2>
-                <div align="center"><img src="https://s3-us-west-2.amazonaws.com/albertoal-assets/codecore_logo.jpg" width="400" height="400"></div>
+                <div align="center"><img src="https://s3-us-west-2.amazonaws.com/awsfordevs-assets/codecore_logo.jpg" width="400" height="400"></div>
                 <p>Underlying instance hostname: <?php echo gethostname(); ?></p>
                 <p>Underlying instance PHP version <?php echo phpversion(); ?></p>
         </div>
